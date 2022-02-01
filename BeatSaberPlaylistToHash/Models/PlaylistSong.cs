5f364f0e86e5e53b2cabe1acc2cc0065c3552bdb
Fuyuki Sakura <fuyuki@sakura.live>
2022-02-01 14:08:01 +1000

    add playlist object skeleton

---
+++ BeatSaberPlaylistToHash/Models/PlaylistSong.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BeatSaberPlaylistToHash.Models
+{
+    /// <summary>
+    /// Represents the metadata of the song in the playlist
+    /// </summary>
+    public class PlaylistSong
+    {
+        /// <summary>
+        /// Gets or sets the hash code of a song in playlist
+        /// </summary>
+        public string Hash { get; set; }
+    }
+}
