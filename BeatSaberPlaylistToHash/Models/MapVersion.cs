7b0d6ea2f5358031b3dbf1a564838793d2a26ef0
Fuyuki Sakura <fuyuki@sakura.live>
2022-02-01 14:29:34 +1000

    remove unused imports

--- BeatSaberPlaylistToHash/Models/MapVersion.cs
+++ BeatSaberPlaylistToHash/Models/MapVersion.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeatSaberPlaylistToHash.Models
+﻿namespace BeatSaberPlaylistToHash.Models
 {
     /// <summary>
     /// Represents the metadata version of the map
